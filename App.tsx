43ac8bac22d457175902016c9597da939ab4025e
Federico Valles <federico.valles@cookunity.com>
2023-09-13 11:19:08 +0200

    feat: styled components installed and colors theme created

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {Home} from './src/screens/home';
+import {ThemeProvider} from 'styled-components';
+import {Colors} from './src/theme/colors';
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <Home />
+    <ThemeProvider theme={{Colors}}>
+      <Home />
+    </ThemeProvider>
   </QueryClientProvider>
 );
 
---
+++ src/theme/colors.ts
@@ -0,0 +1,10 @@
+// names based on https://colornamer.robertcooper.me/
+
+export const Colors = {
+  /** White */
+  white: '#FFFFFF',
+  /** Black */
+  black: '#000000',
+  /** Grays */
+  gondola: '#383333',
+};
