7adb1cd03562cc1e08009367cad46ef77b77238d
Federico Valles <federico.valles@cookunity.com>
2023-09-14 21:45:18 +0200

    feat: splash screen implemented

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {ThemeProvider} from 'styled-components';
 import {Colors} from './src/theme/colors';
 import {Spacers} from './src/theme/spacers';
 import {NavigationContainer} from '@react-navigation/native';
 import {HomeStack} from './src/stacks/home-stack';
+import SplashScreen from 'react-native-splash-screen';
 
 const queryClient = new QueryClient();
 
@@ -12,14 +13,20 @@ const queryClient = new QueryClient();
  * App
  */
 
-const App = () => (
-  <NavigationContainer>
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={{Colors, Spacers}}>
-        <HomeStack />
-      </ThemeProvider>
-    </QueryClientProvider>
-  </NavigationContainer>
-);
+const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+  return (
+    <NavigationContainer>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={{Colors, Spacers}}>
+          <HomeStack />
+        </ThemeProvider>
+      </QueryClientProvider>
+    </NavigationContainer>
+  );
+};
 
 export default App;
